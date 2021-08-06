29b0de8a4d60cf1b5d4ed69d8ffa14e5b8b5755a
haiderasif1 <mhaider92270@gmail.com>
2021-08-07 02:19:44 +0500

    python project

---
+++ data.py
@@ -0,0 +1,4 @@
+class DATA():
+    def __init__(self, id, password):
+        self.id = id
+        self.password = password
---
+++ login.py
@@ -0,0 +1,34 @@
+from data import DATA
+
+
+class system(DATA):
+    pass
+
+
+condition = "yes"
+while condition == "yes":
+    print("\t Login and signup system\n")
+    print("1: To Register\n")
+    print("2: To Login\n")
+    choice = int(input("Enter your option: "))
+    if choice == 1:
+        file=open("F:/python/file.txt","w")
+        id=input("Enter your id: ")
+        password=input("Enter your password: ")
+        file.write(id)
+        file.write("\n")
+        file.write(password)
+        file.close()
+    elif choice == 2:
+        file=open("F:/python/file.txt").read().split('\n')
+        s=system(file[0],file[1])
+        id1=input("Enter your id:")
+        password1=input("Enter your password:")
+        if id1 == s.id and password1==s.password:
+            print("Logged in sucessfully")
+        else:
+            print("Failed to login")
+    else:
+        print("Wrong input")
+    condition=input("Press yes to try again: ")
+
